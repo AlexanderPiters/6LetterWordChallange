28a37383e648d0987685f854201377ec44f05576
Alexander Piters <alpistudios@gmail.com>
2022-01-24 21:29:49 +0100

    Created new project

---
+++ 6LetterWordChallange/6LetterWordChallange/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _6LetterWordChallange.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
